25a8843d9f9eafa043b8aff1a2be6d0d80201d6b
dylvaz <vazquezadylan@gmail.com>
2021-03-03 21:36:53 -0800

    implemented notFound and errHandler middleware
    - notFound will return a 404 code and a message stating that the route trying to be accessed does not exist
    - if any request makes it through with a status code 200 and has not been handled it will be handled by the errHandler to return the err, and be given a status code 500 unless it's original code was not 200.

---
+++ server/util/middlewares.js
@@ -0,0 +1,19 @@
+const notFound = (req, res, next) => {
+  const err = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(err);
+};
+
+const errHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack,
+  });
+};
+
+module.exports = {
+  errHandler,
+  notFound,
+};
