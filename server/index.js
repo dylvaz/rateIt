7f81a28e1ddec5ef312c77872e746d8446841cea
dylvaz <vazquezadylan@gmail.com>
2021-03-03 21:33:22 -0800

    implemented API route handlers and express app
    - get request for retrieving a specific company
    - get request for retrieving all companies
    - post request for creating a company
    - patch request for updating an existing company
    - delete request for deleting a company entry

---
+++ server/index.js
@@ -0,0 +1,104 @@
+const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+require('dotenv').config();
+
+const db = require('./db/');
+const { errHandler, notFound } = require('./util/middlewares');
+
+const app = express();
+const port = process.env.PORT || 1234;
+
+//Middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+app.use(helmet());
+app.use(cors());
+
+//Get all companies
+app.get('/api/v1/companies', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM companies');
+    res.status(200).json({
+      results: results.rows.length,
+      data: {
+        companies: results.rows,
+      },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//Get one company
+app.get('/api/v1/companies/:id', async (req, res) => {
+  try {
+    const results = await db.query('SELECT * FROM companies WHERE id = $1', [
+      req.params.id,
+    ]);
+    res.status(200).json({
+      results: results.rows.length,
+      data: {
+        company: results.rows[0],
+      },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//Create a review for a company
+app.post('/api/v1/companies', async (req, res) => {
+  try {
+    const results = await db.query(
+      'INSERT INTO companies (name, location, price_range) VALUES ($1, $2, $3) RETURNING *',
+      [req.body.name, req.body.location, req.body.price_range]
+    );
+    res.status(201).json({
+      results: results.rows.length,
+      data: {
+        company: results.rows[0],
+      },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//Update a companies review
+app.patch('/api/v1/companies/:id', async (req, res) => {
+  try {
+    const results = await db.query(
+      'UPDATE companies SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *',
+      [req.body.name, req.body.location, req.body.price_range, req.params.id]
+    );
+    res.status(201).json({
+      results: results.rows.length,
+      data: {
+        company: results.rows[0],
+      },
+    });
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//Delete a company review
+app.delete('/api/v1/companies/:id', async (req, res) => {
+  try {
+    await db.query('DELETE FROM companies WHERE id = $1', [req.params.id]);
+    res.status(200).send('Succesfully deleted. 🚀');
+  } catch (err) {
+    throw new Error(err);
+  }
+});
+
+//More middlewares
+app.use(notFound);
+app.use(errHandler);
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port}`);
+});
