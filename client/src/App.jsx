2af1c9452f1ae463d2f61b0a1eddd59577b34fe1
dylvaz <vazquezadylan@gmail.com>
2021-03-05 15:51:52 -0800

    implemented Router, Switch, and Routes using react-router-dom
    - '/' path will render the Home component
    - '/companies/:id/update' path will render the Update Company component
    - '/companies/:id' path will render the Company Details component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,35 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import { CompaniesContextProvider } from './context/CompaniesContext';
+import CompanyDetails from './pages/CompanyDetails';
+import Home from './pages/Home';
+import UpdateCompany from './pages/UpdateCompany';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <CompaniesContextProvider>
+      <div
+        style={{
+          background: 'linear-gradient(to bottom, #6600cc 0%, #ff9966 100%)',
+          width: '100vw',
+          height: '100vh',
+        }}
+      >
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route
+              exact
+              path='/companies/:id/update'
+              component={UpdateCompany}
+            />
+            <Route exact path='/companies/:id' component={CompanyDetails} />
+          </Switch>
+        </Router>
+      </div>
+    </CompaniesContextProvider>
+  );
 };
 
 export default App;
