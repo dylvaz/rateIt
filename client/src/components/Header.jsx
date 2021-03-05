9d0791bbd3806be1b9fe8c102163fb8af11a1230
dylvaz <vazquezadylan@gmail.com>
2021-03-05 15:56:22 -0800

    implemented header text and styled using bootstrap

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <div>
+      <h1
+        className='fw-light display-1 text-center'
+        style={{ color: 'whitesmoke' }}
+      >
+        Company Reviewer
+      </h1>
+    </div>
+  );
+};
+
+export default Header;
