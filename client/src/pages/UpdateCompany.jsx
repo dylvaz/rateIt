d71144a302c58f33512ddf138cd2b186196e9d7c
dylvaz <vazquezadylan@gmail.com>
2021-03-05 18:34:36 -0800

    added a header and hr with styling :art: :crayon:

--- client/src/pages/UpdateCompany.jsx
+++ client/src/pages/UpdateCompany.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
 const UpdateCompany = () => {
-  return <div>UpdateCompany</div>;
+  return (
+    <>
+      <h1
+        className='fw-lighter display-1 text-center'
+        style={{ paddingTop: '30px' }}
+      >
+        Update Restaurant
+      </h1>
+      <hr style={{ marginBottom: '30px' }}></hr>
+    </>
+  );
 };
 
 export default UpdateCompany;
