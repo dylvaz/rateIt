608fc1f2fe809af61f33033bd3ac95eb0ea3216a
dylvaz <vazquezadylan@gmail.com>
2021-03-08 22:07:50 -0800

    implemented the selectedCompany state and passed it the app's context

--- client/src/context/CompaniesContext.js
+++ client/src/context/CompaniesContext.js
@@ -4,11 +4,20 @@ export const CompaniesContext = createContext();
 
 export const CompaniesContextProvider = (props) => {
   const [companies, setCompanies] = useState([]);
+  const [selectedCompany, setSelectedCompany] = useState([]);
   const addCompany = (company) => {
     setCompanies([...companies, company]);
   };
   return (
-    <CompaniesContext.Provider value={{ companies, setCompanies, addCompany }}>
+    <CompaniesContext.Provider
+      value={{
+        companies,
+        setCompanies,
+        addCompany,
+        selectedCompany,
+        setSelectedCompany,
+      }}
+    >
       {props.children}
     </CompaniesContext.Provider>
   );
