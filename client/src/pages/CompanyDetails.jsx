8cc0857ecef10462af448da51e40980f7570a515
dylvaz <vazquezadylan@gmail.com>
2021-03-08 22:11:10 -0800

    added a useEffect hook that fetches the data for the selected company

--- client/src/pages/CompanyDetails.jsx
+++ client/src/pages/CompanyDetails.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
+
+import CompanyFinder from '../apis/CompanyFinder';
+import { CompaniesContext } from '../context/CompaniesContext';
 
 const CompanyDetails = () => {
-  return <div>CompanyDetails</div>;
+  const { selectedCompany, setSelectedCompany } = useContext(CompaniesContext);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await CompanyFinder.get(`/${id}`);
+        setSelectedCompany(response.data.data.company);
+      } catch (err) {
+        throw new Error(err);
+      }
+    };
+    fetchData();
+  }, [id, setSelectedCompany]);
+
+  return <div>{selectedCompany && selectedCompany.name}</div>;
 };
 
 export default CompanyDetails;
