c83f852d55e6858548e623982083016d080db4c5
dylvaz <vazquezadylan@gmail.com>
2021-03-13 22:20:12 -0800

    fixed styliing to prevent reviews from rendering on different lines

--- client/src/components/Reviews.jsx
+++ client/src/components/Reviews.jsx
@@ -5,26 +5,26 @@ import StarRating from './StarRating';
 const Reviews = ({ reviews }) => {
   return (
     <div className='container-md'>
-      {reviews &&
-        reviews.map((review) => {
-          return (
-            <div
-              className='card text-white bg-primary mb-3 me-4'
-              style={{ maxWidth: '30%' }}
-              key={review.id}
-            >
-              <div className='card-header d-flex justify-content-between'>
-                <span>{review.name}</span>
-                <span>
-                  <StarRating rating={review.rating} />
-                </span>
+      <div className='row'>
+        {reviews &&
+          reviews.map((review) => {
+            return (
+              <div className='col-4' key={review.id}>
+                <div className='card text-white bg-primary mb-3'>
+                  <div className='card-header d-flex justify-content-between'>
+                    <span>{review.name}</span>
+                    <span>
+                      <StarRating rating={review.rating} />
+                    </span>
+                  </div>
+                  <div className='card-body'>
+                    <p className='card-text'>{review.body}</p>
+                  </div>
+                </div>
               </div>
-              <div className='card-body'>
-                <p className='card-text'>{review.body}</p>
-              </div>
-            </div>
-          );
-        })}
+            );
+          })}
+      </div>
     </div>
   );
 };
